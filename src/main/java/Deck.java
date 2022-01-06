6a68fdce945879a241c15b2e1121b37df31aa2c9
adrianartale <23aartale@athenian.org>
2022-01-05 20:09:00 -0800

    donezo

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 // initialize, shuffle, deal, and check if empty.
 public class Deck {
 	// cards is an ArrayList of all the Card objects in our deck
-	private ArrayList<Card> cardsList;
+	private ArrayList <Card> cardsList;
 
 	// size is the number of not-yet-dealt cards.
 	// Cards are dealt from the top (highest index) down.
@@ -16,7 +16,15 @@ public class Deck {
 	// You will need to initialize both the cardsList and size instance variables
 	// You should go through and make all possible pairs of suits and ranks
 	public Deck(String[] ranks, String[] suits, int[] values) {
-		// YOUR CODE HERE
+		cardsList = new ArrayList<Card>();
+		size = ranks.length * suits.length;
+
+		for(int i = 0; i< suits.length; i++){
+			for (int j = 0; j< ranks.length; j++){
+				Card c = new Card (ranks[j],suits[i], values[j]);
+				cardsList.add(c);
+			}
+		}
 		// // Remember, in a constructor you need to first create the ArrayList for the instance variable!
 	}
 	// Deals a card from this deck.
@@ -24,28 +32,36 @@ public class Deck {
 	// Recall that the cards are dealt from top (highest-index) down
 	// Updates the size as well
 	public Card deal() {
-		// YOUR CODE HERE
+		if (size > 0) {
+			size--;
+			return cardsList.get(size);
+		}
         return null;
 	}
 
 	// Determines if this deck is empty (there are no undealt cards).
 	// returns true if this deck is empty, false otherwise.
 	public boolean isEmpty() {
-		// YOUR CODE HERE
+		if (size > 0){
+			return true;
+		}
 		return false;
 	}
 
 	// Returns the size (number of undealt cards) in this deck.
 	public int getSize() {
-		// YOUR CODE HERE
-		return -1;
+		return size;
 	}
 
 	// Shuffles the deck by repeatedly randomly swapping pairs of cards
 	// This method should change the order of the cards in cardsList
 	// Shuffling should also reset the size variable to its original value
 	public void shuffle() {
-		// YOUR CODE HERE
+		int size = cardsList.size();
+		for (int i = 0; i< size; i++){
+			int rando = (int) (Math.random() * size);
+			cardsList.set(rando, cardsList.get(i));
+		}
 	}
 
 	// OPTIONAL: Write code that carries out a "perfect" shuffle
